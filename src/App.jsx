22adbb0fd91e50cebff591cb4d9e4b288f61ef7a
Sonia William <98wsonia@gmail.com>
2024-09-14 16:50:26 +0800

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from './pages/HomePage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl text-secondary font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
