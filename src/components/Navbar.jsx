2ed32b1399afd37d5bc9bbb580e2ccf55745b49f
Sonia William <98wsonia@gmail.com>
2024-09-15 13:54:56 +0800

    add expandable menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,29 +22,56 @@ const Navbar = () => {
 
 	return (
 		<>
-			<div className={`global-border navbar-div ${isCartOpen ? "expanded-navbar" : ""}`}>
+			<div
+				className={`global-border navbar-div ${
+					isCartOpen ? "expanded-navbar" : ""
+				}`}
+			>
 				<div className="navbar-content">
 					<div className="navbar-start">
 						<a className="navbar-logo">anythingStore&trade;</a>
 					</div>
 					<div className="navbar-end">
 						<ul className="navbar-items-container">
-							<NavbarIcon iconClass="person-fill" />
-							<NavbarIcon iconClass="cart-check-fill" onMouseEnter={handleMouseEnter} />
-							<li>
-								<a>Menu</a>
-							</li>
+							{!isCartOpen && (
+								<li>
+									<details>
+										<summary>Menu</summary>
+										<ul className="global-border bg-white w-40 right-0">
+											<li>
+												<a>Shop</a>
+											</li>
+											<li>
+												<a>About Us</a>
+											</li>
+											<li>
+												<a>Blog</a>
+											</li>
+										</ul>
+									</details>
+								</li>
+							)}
+							{!isCartOpen && (
+								<NavbarIcon iconClass="person-fill" />
+							)}
+							<NavbarIcon
+								iconClass="cart-check-fill"
+								onMouseEnter={handleMouseEnter}
+							/>
 						</ul>
 					</div>
 				</div>
 
 				{/* Cart Summary */}
 				{isCartOpen && (
-					<div className="expanded-content" onMouseLeave={handleMouseLeave}>
+					<div
+						className="expanded-content"
+						onMouseLeave={handleMouseLeave}
+					>
 						<div className="cart-summary">
-                            <h2>Your Cart</h2>
-                            <p>No items in your cart.</p>
-                        </div>
+							<h2>Your Cart</h2>
+							<p>No items in your cart.</p>
+						</div>
 					</div>
 				)}
 			</div>
